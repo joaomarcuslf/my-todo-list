0ddc89dcc626537bb78c5081ad4157c4c52210f5
João Marcus de Lemos Fernandes <jmpierrotfernandes@hotmail.com>
2016-03-24 23:07:02 -0300

    First features tests added

---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "Creating todo lists" do
+	it "redirects to the todo list index page on success" do
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New Todo List")
+
+		fill_in "Title", with: "My List"
+		fill_in "Description", with: "My test list"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("My List")
+	end
+
+	it "displays an error when the todo list has no title" do
+		expect(TodoList.count).to be_zero
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New Todo List")
+
+		fill_in "Title", with: ""
+		fill_in "Description", with: "My test list"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to be_zero
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("My test list")		
+	end
+end
